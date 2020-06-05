aaa02c5de431008c477960008d8a11eb29299115
Ronald <ronald.scruz16@gmail.com>
2020-06-05 07:17:12 -0300

    Removed old database setup files

--- backend/src/models/UserRoles.ts
+++
@@ -1,7 +0,0 @@
-import { Sequelize } from 'sequelize/types';
-
-module.exports = (sequelize: any, DataTypes: any) => {
-  const UserRoles = sequelize.define('UserRoles', {});
-
-  return UserRoles;
-};
